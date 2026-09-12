5fe743bb629bb158d69070d581f0413531b06cf4
Norah-444 <nourabn215@gmail.com>
2026-09-12 13:59:38 +0300

    Update constants.dart

--- flutter/lib/core/constants.dart
+++ flutter/lib/core/constants.dart
@@ -1,6 +1,5 @@
 class AppConstants {
   // TODO: Change this IP before running on a physical device.
 
-  static const String baseUrl = 'http://172.20.10.2:3000';
+  static const String baseUrl = 'http://10.0.2.2:3000';
 }
-
